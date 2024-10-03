10fb5ac20bbecb2a9de875e8e1b7ac3694142fd6
George Pacheco Pinto <georgepacheco1@gmail.com>
2024-10-03 13:21:21 -0300

    .

--- solid_server/FotSolid/Sensor2Gateway/src/Environment.ts
+++ solid_server/FotSolid/Sensor2Gateway/src/Environment.ts
@@ -1,10 +1,10 @@
 
 export const Environment = {
 
-    WEBID: "http://147.79.82.127:3000/HomeSimulator/profile/card#me",
+    WEBID: "https://147.79.82.127:3000/HomeSimulator/profile/card#me",
     // WEBID: "https://192.168.0.111:3000/HomeSimulator/profile/card#me",
     PODNAME: "HomeSimulator",
-    IDP: "http://147.79.82.127:3000/",
+    IDP: "https://147.79.82.127:3000/",
     // IDP: "https://192.168.0.111:3000/",
     LOGIN: "home_simulator@fotsimulator.com",
     PASSWORD: "simulator2024",

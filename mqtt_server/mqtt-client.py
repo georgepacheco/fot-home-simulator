d4dff2e5c3a11634a0967a7f705e445ac07634fd
George Pacheco Pinto <georgepacheco1@gmail.com>
2025-04-23 10:22:45 -0300

    .

--- mqtt_server/mqtt-client.py
+++ mqtt_server/mqtt-client.py
@@ -5,6 +5,9 @@ import logging
 import shutil
 import send_data_server
 import os
+import json
+from datetime import datetime
+import pytz
 
 # MQTT broker settings
 broker_address = sys.argv[1]
@@ -12,16 +15,42 @@ server_ip = sys.argv[2]
 broker_port = 1883
 topic = '#'
 output_file = 'data.json'
+permanent_output_file = 'data_all.json'
 file_path = "send-data.json"
 time_send = 30
 
 
+# # Callback function for handling received messages
+# def on_message(client, userdata, message):
+#     with open(output_file, 'a') as file:
+#         logging.info(message.payload.decode() + '\n')
+#         file.write(message.payload.decode() + '\n')
+
 # Callback function for handling received messages
 def on_message(client, userdata, message):
-    with open(output_file, 'a') as file:
-        logging.info(message.payload.decode() + '\n')
-        file.write(message.payload.decode() + '\n')
+    try:
+        # Decodifica a mensagem e converte para um dicionário
+        msg_dict = json.loads(message.payload.decode())
 
+        # Atualiza o campo 'datetime_pub' para a data e hora atuais
+        # msg_dict["datetime_pub"] = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+        tz = pytz.timezone("America/Sao_Paulo")
+        msg_dict["datetime_pub"] = datetime.now(tz).strftime("%Y-%m-%d %H:%M:%S")
+
+        # Converte novamente para JSON
+        updated_message = json.dumps(msg_dict)
+
+        # Escreve no arquivo temporario
+        with open(output_file, 'a') as file:
+            logging.info(updated_message + '\n')
+            file.write(updated_message + '\n')
+            
+        # Escreve no arquivo permanente
+        with open(permanent_output_file, 'a') as perm_file:
+            perm_file.write(updated_message + '\n')
+
+    except json.JSONDecodeError as e:
+        logging.error(f"Erro ao decodificar JSON: {e}")
 
 def on_connect(client, userdata, flags, rc):
     # Subscribe to the topic
